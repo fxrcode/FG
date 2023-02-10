a614580e8fc29de20fe47eb0963d398cf03cc117
fxrc.cs <fxrcode@users.noreply.github.com>
2023-02-10 08:12:28 -0800

    Time: 601 ms (69.91%), Space: 16.5 MB (5.04%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.py
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.py
@@ -1,19 +1,11 @@
 class Solution:
     def maxDistance(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
-        q = deque([(i, j) for i in range(m) for j in range(n) if grid[i][j] == 1])
-        if len(q) == m * n:
-            return -1
-        seen = set(q)
-        step = 0
-        while q:
-            for _ in range(len(q)):
-                x, y = q.popleft()
-                for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-                    xx, yy = x + dx, y + dy
-                    if 0 <= xx < m and 0 <= yy < n and grid[xx][yy] == 0 and (xx, yy) not in seen:
-                        q.append((xx, yy))
-                        seen.add((xx, yy))
-            step += 1
-
-        return step - 1
+        n, res = len(grid), 0
+        land = {(i, j) for i, j in product(range(n), range(n)) if grid[i][j]}
+        water = {(i, j) for i, j in product(range(n), range(n)) if not grid[i][j]}
+        while water:
+            if not land: return -1
+            land = {(x, y) for i, j in land for x, y in ((i+1, j), (i-1, j), (i, j+1), (i, j-1)) if (x, y) in water}
+            water -= land
+            res += 1
+        return res or -1
