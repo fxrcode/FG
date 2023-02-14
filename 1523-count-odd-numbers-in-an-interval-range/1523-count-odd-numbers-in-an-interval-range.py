c92a9a6b49b43167ee461b7381e85b0fb19cd277
fxrc.cs <fxrcode@users.noreply.github.com>
2023-02-13 17:25:17 -0800

    Time: 33 ms (57.04%), Space: 13.8 MB (50.16%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.py
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.py
@@ -1,3 +1,5 @@
 class Solution:
     def countOdds(self, low: int, high: int) -> int:
-        return (high + 1) // 2 - low // 2
+        if low % 2 == 0:
+          return (high-low+1)//2
+        return (high-low)//2 + 1
