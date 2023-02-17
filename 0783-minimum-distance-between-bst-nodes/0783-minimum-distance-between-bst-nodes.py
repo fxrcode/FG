e8b631e4bfec93acb926ab5c97d52e1630bc78be
fxrc.cs <fxrcode@users.noreply.github.com>
2023-02-16 18:18:30 -0800

    Time: 33 ms (67.95%), Space: 13.9 MB (32.63%) - LeetHub

---
+++ 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDiffInBST(self, root: Optional[TreeNode]) -> int:
+        mn = float("inf")
+        prev: Optional[TreeNode] = None
+
+        def ino(T: TreeNode):
+            if not T:
+                return 
+            ino(T.left)
+            nonlocal prev, mn
+            if prev:
+                mn = min(mn, T.val - prev.val)
+            prev = T
+            ino(T.right)
+
+        ino(root)
+        return mn
