67fcdc7c2d37658ee5511b5c759cae5ee9942b90
fxrc.cs <fxrcode@users.noreply.github.com>
2023-02-21 17:41:19 -0800

    Time: 484 ms (75.23%), Space: 17 MB (78.06%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -1,20 +1,21 @@
 class Solution:
     def shipWithinDays(self, weights: List[int], days: int) -> int:
-        def feasible(x):
-            c = 0
+        def feasible(capacity) -> bool:
+            """
+            https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/discuss/769698/Python-Clear-explanation-Powerful-Ultimate-Binary-Search-Template.-Solved-many-problems.
+            zhijun_liao feasible is more cleaner than mine
+            """
             d = 1
+            tot = 0
             for w in weights:
-                if c + w > x:
-                    c = w
+                tot += w
+                if tot > capacity:  # too heavy, wait for the next day
+                    tot = w
                     d += 1
-                else:
-                    c += w
-                if d > days:
-                    return False
-            if d <= days:
-                return True
-            return False
-
+                    if d > days:  # cannot ship within D days
+                        return False
+            return True
+        
         l, r = max(weights), sum(weights) + 1
         while l < r:
             m = (l + r) // 2
