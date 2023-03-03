c262f48ae723d4f1c132364c0dddbb6215745cad
fxrc.cs <fxrcode@users.noreply.github.com>
2023-03-03 12:58:53 -0800

    Time: 39 ms (24.97%), Space: 13.9 MB (54.39%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -0,0 +1,22 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        def kmp(str_):
+            b, prefix = 0, [0]
+            for i in range(1, len(str_)):
+                while b > 0 and str_[i] != str_[b]:
+                    b = prefix[b - 1]
+                if str_[b] == str_[i]:
+                    b += 1
+                else:
+                    b = 0
+                prefix.append(b)
+            return prefix
+
+        str_ = kmp(needle + '#' + haystack)
+        n = len(needle)
+        if n == 0:
+            return n
+        for i in range(n + 1, len(str_)):
+            if str_[i] == n:
+                return i - 2 * n
+        return -1
