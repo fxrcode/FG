03de3ea5376e4369bf3e7173d943c4b01ffc4ef9
fxrc.cs <fxrcode@users.noreply.github.com>
2023-04-18 16:08:59 -0700

    Time: 29 ms (80.23%), Space: 13.9 MB (54.67%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -3,5 +3,6 @@ class Solution:
         res = []
         for i in range(min(len(word1), len(word2))):
             res.append(word1[i] + word2[i])
+        # !for i in range(5). i=4 after loop! So I need to word1[i+1:] rather word1[i:]
         res.append(word1[i + 1 :] + word2[i + 1 :])
         return "".join(res)
