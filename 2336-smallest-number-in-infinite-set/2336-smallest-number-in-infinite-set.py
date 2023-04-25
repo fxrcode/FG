de2dc15a395ea0ae6390373bac34813810d621fa
fxrc.cs <fxrcode@users.noreply.github.com>
2023-04-24 20:41:39 -0700

    Time: 117 ms (83.24%), Space: 14.6 MB (56.04%) - LeetHub

---
+++ 2336-smallest-number-in-infinite-set/2336-smallest-number-in-infinite-set.py
@@ -0,0 +1,24 @@
+class SmallestInfiniteSet:
+
+    def __init__(self):
+        self.curr = 1
+        self.set = set()
+
+    def popSmallest(self) -> int:
+        if self.set:
+            ans = min(self.set)
+            self.set.remove(ans)
+            return ans
+        else: 
+            self.curr += 1
+            return self.curr -1
+
+    def addBack(self, num: int) -> None:
+        if self.curr > num:
+            self.set.add(num)
+
+
+# Your SmallestInfiniteSet object will be instantiated and called as such:
+# obj = SmallestInfiniteSet()
+# param_1 = obj.popSmallest()
+# obj.addBack(num)
