e5787407ecae5cc5c6eabf5523b10cc3156327f3
fxrc.cs <fxrcode@users.noreply.github.com>
2023-05-01 18:48:17 -0700

    Time: 50 ms (5.34%), Space: 16.2 MB (5.29%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.py
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.py
@@ -1,8 +1,8 @@
 class Solution:
     def average(self, salary: List[int]) -> float:
-        mn, mx, tot = 2e9, 0, 0
+        mn, mx, tot = float("inf"), 0, 0
         for s in salary:
-            tot += s
             mn = min(mn, s)
             mx = max(mx, s)
-        return (tot - mn - mx) / (len(salary)-2)
+            tot += s
+        return (tot - mn - mx) / (len(salary) - 2)
