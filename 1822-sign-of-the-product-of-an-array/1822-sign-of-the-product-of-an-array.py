a1c2596758cc461cc3ac9051ab4cad9b9bbddd65
fxrc.cs <fxrcode@users.noreply.github.com>
2023-05-01 18:40:09 -0700

    Time: 73 ms (7.28%), Space: 16.4 MB (7.53%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.py
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.py
@@ -1,9 +1,3 @@
 class Solution:
     def arraySign(self, nums: List[int]) -> int:
-        sign = 1
-        for n in nums:
-            if n == 0:
-                return 0
-            if n < 0:
-                sign *= -1
-        return sign
+        return 0 if 0 in nums else -1 if sum(x < 0 for x in nums) % 2 else 1
